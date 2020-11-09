a5def7a771fe8dafc722f5f5443c52d08b4e1696
jyrj <jayarajevur@gmail.com>
2020-11-09 22:23:03 +0530

    removed babel, moment;

--- src/database/models.py
+++ src/database/models.py
@@ -4,15 +4,10 @@
 import os
 from flask_migrate import Migrate
 import json
-import dateutil.parser
-import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from sqlalchemy import Column, String, Integer
-from logging import Formatter, FileHandler
-from flask_wtf import Form
 from forms import *
 #----------------------------------------------------------------------------#
 # App Config.
