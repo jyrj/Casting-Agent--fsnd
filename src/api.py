47f8e4b9b4fe14a2acd1567583d41f1e05427cdd
jyrj <jayarajevur@gmail.com>
2020-11-19 01:43:50 +0530

    Model finalized

--- src/api.py
+++ src/api.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import os
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+from flask_cors import CORS
+
+from .database.models import db_drop_and_create_all, setup_db, Movie
+#from .auth.auth import AuthError, requires_auth
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+db_drop_and_create_all
+
+# ROUTES
--- src/database/models.py
+++ src/database/models.py
@@ -30,3 +30,72 @@ db_drop_and_create_all()
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
+'''
+Movies
+a persistent movies entity with title and release date as attributes, extends the base SQLAlchemy Model
+'''
+
+class Movie(db.Model):
+    __tablename__= "movies"
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String(80), unique=True)
+    release_date =  Column(db.DateTime())
+    #connection with other model
+
+    def movies_model(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "release date": self.release_date
+        }
+
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            movie = Movie(title=req_title, release_date=release date)
+            movie.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie(title=req_title, release_date=release date)
+            movie.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie.query.filter(Movie.id == id).one_or_none()
+            movie.title = '<New Name>''
+            movie.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.movies_model())
+
+class Actors(db.Model):
+    __tablename__="actors"
+    
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(80))
+    age = Column(Integer())
+    gender = Column(String(26))
+    #backref
