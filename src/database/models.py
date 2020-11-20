a689db2f95c46f6baf8b375a073d045c90d130aa
jyrj <jayarajevur@gmail.com>
2020-11-20 22:46:03 +0530

    added environment variable

--- src/database/models.py
+++ src/database/models.py
@@ -5,7 +5,7 @@ import json
 
 #database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "postgres://postgres:@localhost:5432/movies"
+database_path = "postgres://bvrlpffleqganr:ed2f4bd02acae685c787adbbb555fca5cd4643f2a544c545dd453b6c369bd8ce@ec2-3-216-92-193.compute-1.amazonaws.com:5432/dd6rfv7k6b0hmk"
 
 db = SQLAlchemy()
 
